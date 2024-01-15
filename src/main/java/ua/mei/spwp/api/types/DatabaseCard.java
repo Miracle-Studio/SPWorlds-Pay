21961265b1f36093befb647efd87e6357df65026
MeiNanziiii <meinanziiii.ua@gmail.com>
2024-01-15 22:50:16 +0200

    гитхаб не залил этот файл бля

--- src/main/java/ua/mei/spwp/api/types/DatabaseCard.java
+++ src/main/java/ua/mei/spwp/api/types/DatabaseCard.java
@@ -1,7 +1,13 @@
 package ua.mei.spwp.api.types;
 
+import net.minecraft.client.gui.tooltip.*;
+import net.minecraft.registry.*;
+import net.minecraft.text.*;
 import net.minecraft.util.*;
 import ua.mei.spwp.api.*;
+import ua.mei.spwp.client.*;
+import ua.mei.spwp.client.screens.or.*;
+import ua.mei.spwp.client.screens.or.components.*;
 
 import java.util.*;
 
@@ -12,5 +18,25 @@ public record DatabaseCard(int rowid, String name, Identifier texture, String id
 
     public Card card() {
         return SPWorldsApi.requestBalance(this);
+    }
+
+    public FakeSlot asSlot() {
+        List<Text> tooltips = new ArrayList<>();
+        tooltips.add(Text.literal(this.name).styled(style -> style.withColor(ORColorScheme.GRAY)));
+        tooltips.add(Text.literal("• Баланс: загрузка...").styled(style -> style.withColor(ORColorScheme.DARK_GRAY)));
+
+        FakeSlot slot = new FakeSlot(Registries.ITEM.get(this.texture).getDefaultStack(), tooltips);
+
+        SPWorldsPayClient.tasks.addTask(this::card, result -> {
+            Card card = (Card) result;
+
+            tooltips.set(1, Text.literal("• Баланс: " + card.balance()).styled(style -> style.withColor(ORColorScheme.GRAY)));
+            slot.tooltip(tooltips);
+            slot.notifyParent();
+        }, exception -> {
+
+        });
+
+        return slot;
     }
 }
